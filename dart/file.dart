0e9de1b8d5bb09ce46771ef5a8618c62dd3b3f6f
futabooo <takahiro.futagawa@gmail.com>
2021-01-14 14:02:39 +0900

    modify file.dart

--- dart/file.dart
+++ dart/file.dart
@@ -3,11 +3,14 @@ import 'dart:io';
 /// 指定したディレクトリのファイル一覧を取得する
 Future<void> main(List<String> arguments) async {
   final directory = Directory('/Users/futabooo');
-  final fileNames = directory.list(
-    recursive: true, // ディレクトリを再帰的に探索しない
-    followLinks: false, // シンボリックリンクをファイルとして扱わない
-  );
+  final fileNames = directory
+      .list(
+        recursive: false, // ディレクトリを再帰的に探索しない
+        followLinks: false, // シンボリックリンクをファイルとして扱わない
+      )
+      .where((entity) => entity is File)
+      .map((entity) => entity.path);
   await for (final fileName in fileNames) {
-    if (fileName is File) print(fileName.path);
+    print(fileName);
   }
 }
